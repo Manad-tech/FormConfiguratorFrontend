63f3d13de13fd4240eba3a17d4918a646f908093
Manad-tech <joshimanad81@gmail.com>
2026-04-15 20:05:07 +0530

    feat: added field card component

--- src/features/form-builder/components/FieldCard.tsx
+++ src/features/form-builder/components/FieldCard.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { Card, CardContent } from "@/components/ui/card";
+import type { Field } from "../hooks/useFormBuilder";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-const FieldCard = () => {
+type Props = {
+  field: Field;
+  updateField: (id: string, key: string, value: any) => void;
+  deleteField: (id: string) => void;
+};
+
+const FieldCard = ({ field, updateField, deleteField }: Props) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Card>
+      <CardContent className="p-4 flex flex-col gap-3">
+        <Input
+          placeholder="Field label..."
+          value={field.label}
+          onChange={(e) => updateField(field.id, "label", e.target.value)}
+        />
 
-export default FieldCard
+        <Button variant={"destructive"} onClick={() => deleteField(field.id)}>
+          Delete
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default FieldCard;
